c63efa2bcabc5cfca0ea38ba99ebefce6db9c090
MarcosPrograma <66289881+MarcosPrograma@users.noreply.github.com>
2024-07-12 20:03:42 -0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,6 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
+//Cargar mundo
 export function cargarModelo(scene){
     const loader = new GLTFLoader().setPath('./src/models/');
 
@@ -11,6 +12,7 @@ export function cargarModelo(scene){
     });
 }
 
+//Cargar objetos
 export function cargarObjeto(rutaArchivo, id, scene, posicion) {
     const loader = new GLTFLoader();
 
@@ -18,8 +20,9 @@ export function cargarObjeto(rutaArchivo, id, scene, posicion) {
         console.log('cargando objetos');
         const objeto = gltf.scene;
         objeto.position.set(posicion.x, posicion.y, posicion.z); 
-        objeto.userData.id = id;
+        objeto.userData.id = id; //id para detectar cual panel se abre
 
+        //----Muy importante agregarle una mesh, dado que si no no intersecciona
         objeto.traverse((child) => {
             if (child.isMesh){
                 child.userData.id = id;
